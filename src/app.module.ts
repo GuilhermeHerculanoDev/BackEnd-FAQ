863bfecfdc326b657bb75f80324b488b9d98d8ad
Guilherme <guilhermeherculanodevs@gmail.com>
2024-10-08 15:58:18 -0300

    :sparkles: feat: Criação do módulo de autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { QuestionsController } from './questions/questions.controller';
 import { QuestionsModule } from './questions/questions.module';
 import { AnswersController } from './answers/answers.controller';
 import { AnswersModule } from './answers/answers.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [UsersModule, CategoryModule, QuestionsModule, AnswersModule],
+  imports: [UsersModule, CategoryModule, QuestionsModule, AnswersModule, AuthModule],
   controllers: [AppController, QuestionsController, AnswersController],
   providers: [AppService],
 })
