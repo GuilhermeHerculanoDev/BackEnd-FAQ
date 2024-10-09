03a1e436845dcd71fcd8f15c85d40ce3d4f18d88
Guilherme <guilhermeherculanodevs@gmail.com>
2024-10-09 14:43:35 -0300

    :sparkles: feat: Importando o JwtModule ao AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [JwtModule.registerAsync({
+    global: true,
+    imports: [],
+    useFactory: async (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: {expiresIn: +configService.get<string>('JWT_EXPIRATION_TIME')}
+    }),
+    inject: [ConfigService]
+  })],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthModule]
 })
 export class AuthModule {}
