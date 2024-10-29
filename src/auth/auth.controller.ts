ad84d5121cf3ab33e1a7b58a0ab75f0304323707
Guilherme <guilhermeherculanodevs@gmail.com>
2024-10-29 15:30:31 -0300

    :sparkles: feat: Adicionando o enableCors e o HttpCode

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDTO } from './auth.dto';
 
@@ -8,6 +8,7 @@ export class AuthController {
     constructor (private authService: AuthService) {}
 
     @Post('login')
+    @HttpCode(HttpStatus.OK)
     singIn(@Body('username') username:string, @Body('password') password: string): Promise<AuthDTO>{
         return this.authService.singIn(username, password)
     }
